f31d32e0ed5242e661297e5482bf1a404222b96d
danieldenton <danielmdenton@gmail.com>
2022-09-10 20:48:06 -0700

    loops

--- test.rb
+++ test.rb
@@ -71,9 +71,35 @@ Logical : && || ! and or not
 # end
 
 # ternery
-age = 13
+# age = 13
 
-puts (age >= 50) ? "Old" : "Young"
+# puts (age >= 50) ? "Old" : "Young"
+
+# loops
+
+# x = 1
+# loop do
+#     x += 1
+
+#     next unless (x % 2) == 0
+#     puts x
+
+#     break if x >= 10
+# end
+
+# y = 1
+# while y <= 10
+#     y += 1
+#     next unless (y % 2) == 0
+#     puts y
+# end
+
+a = 1
+until a >= 10
+    a += 1
+    next unless (a % 2) == 0
+    puts a
+end
 
 
 
