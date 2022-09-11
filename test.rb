d9a9f9ba93abd67ca2c6c6f87cd9525c2d47f7e2
danieldenton <danielmdenton@gmail.com>
2022-09-10 19:47:13 -0700

    first commit

---
+++ test.rb
@@ -0,0 +1,7 @@
+print "enter a value: "
+first_num = gets.to_i
+
+print "enter another value: "
+second_num = gets.to_i
+
+puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
