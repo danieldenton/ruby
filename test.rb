5f2f261bd0eaa974b8c9ddba23d860d5aa6c902c
danieldenton <danielmdenton@gmail.com>
2022-09-10 20:13:11 -0700

    test2

--- test.rb
+++ test.rb
@@ -1,7 +1,36 @@
-print "enter a value: "
-first_num = gets.to_i
+# print "enter a value: "
+# first_num = gets.to_i
 
-print "enter another value: "
-second_num = gets.to_i
+# print "enter another value: "
+# second_num = gets.to_i
 
-puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+# puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+
+# # arithmetic operators
+# puts "6 + 4 = " + (6+4).to_s
+# puts "6 - 4 = " + (6-4).to_s
+# puts "6 * 4 = " + (6*4).to_s
+# puts "6 / 4 = " + (6/4).to_s
+# puts "6 % 4 = " + (6%4).to_s
+
+# num_one = 1.000
+# num_two = 0.999
+
+# puts num_one.to_s + " - " + num_two.to_s + " = " + (num_one - num_two).to_s
+
+# puts 1.class
+# puts 1.234.class
+# puts "string".class
+
+# write_handler = File.new("yourSum.out", "w")
+
+# write_handler.puts("Random Text").to_s
+
+# write_handler.close
+
+# data_from_file = File.read("yourSum.out")
+
+# puts "Data from file : " + data_from_file
+
+load "test2.rb"
+
